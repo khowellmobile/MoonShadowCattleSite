df1ffe8670bcb034d33376f0705f22886ae4d84f
howellk3 <khowellmobile@gmail.com>
2023-11-01 10:04:49 -0400

    Added shadow on story card

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,18 +1,10 @@
-import Background from "./Background";
-import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
-import Story from "../content/Story";
 
-import img0 from '../../images/scenicImage0.jpg'
 
 function Layout(props) {
   return (
     <div>
-      <Background backgroundImage={img0}>
-        <MainNavigation />
         <div className={classes.contentDiv}>{props.children}</div>
-      </Background>
-      <Story />
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,17 @@
 import CenterTitle from "../components/content/CenterTitle";
+import Story from "../components/content/Story";
+import Background from "../components/layout/Background";
+import MainNavigation from "../components/layout/MainNavigation";
+import img0 from "../images/scenicImage0.jpg";
 
 function HomePage() {
   return (
     <div>
-      <CenterTitle />
+      <Background backgroundImage={img0}>
+        <MainNavigation />
+        <CenterTitle />
+      </Background>
+      <Story />
     </div>
   );
 }
