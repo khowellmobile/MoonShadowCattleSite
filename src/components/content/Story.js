ee54b47757238ce9b202cf2a637a1193e74f5eac
howellk3 <khowellmobile@gmail.com>
2023-11-06 08:35:39 -0500

    Positioned story elements

--- src/components/content/Story.js
+++ src/components/content/Story.js
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import classes from './Story.module.css'
 
 function Story() {
   return (
     <div className={classes.storyDiv}>
-      <h1>OurStory</h1>
+      <h1 className={classes.title}>OurStory</h1>
+      <p className={classes.text}>Shadyview is the best</p>
+      <Link to="/about" className={classes.link}><u>Read More</u></Link>
     </div>
   );
 }
