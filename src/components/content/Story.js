36be9b0fd09f4ac9ba75a00ecc920406225b2b2b
howellk3 <khowellmobile@gmail.com>
2023-11-06 10:35:34 -0500

    Fixed shadow so it now appears on top and bottom

--- src/components/content/Story.js
+++ src/components/content/Story.js
@@ -5,12 +5,12 @@ function Story() {
   return (
     <div className={classes.storyDiv}>
       <h1 className={classes.title}>OurStory</h1>
-      <p className={classes.text}>
-        <center>
+      <center>
+        <p className={classes.text}>
           A young cattle farm, born from passion and dedication, raises healthy
           cattle sustainably, a promising journey of hard work and growth.
-        </center>
-      </p>
+        </p>
+      </center>
       <Link to="/about" className={classes.link}>
         <u>Read More</u>
       </Link>
