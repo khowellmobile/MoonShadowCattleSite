446a7686b9334913d2051347bb83958d0da84de8
howellk3 <khowellmobile@gmail.com>
2023-11-27 09:00:58 -0500

    Added pictures

--- src/components/content/CardCarousel.js
+++ src/components/content/CardCarousel.js
@@ -1,6 +1,7 @@
 import Carousel from "react-bootstrap/Carousel";
-import img0 from "../../images/scenicImage0.jpg";
-import img1 from "../../images/scenicImage1.jpg";
+import catImg1 from "../../images/Cats.jpg";
+import catImg2 from "../../images/Gertrude.jpg";
+import catImg3 from "../../images/Bambi.jpg";
 import classes from "./CardCarousel.module.css";
 
 function CardCarousel() {
@@ -8,21 +9,21 @@ function CardCarousel() {
     <div className={classes.carousel}>
       <Carousel fade>
         <Carousel.Item>
-          <img className={classes.image} src={img0} alt={"hello"} />
+          <img className={classes.imgPreset2} src={catImg1} alt={"hello"} />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className={classes.image} src={img1} alt={"hello"} />
+          <img className={classes.imgPreset1} src={catImg2} alt={"hello"} />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className={classes.image} src={img0} alt={"hello"} />
+          <img className={classes.imgPreset1} src={catImg3} alt={"hello"} />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
