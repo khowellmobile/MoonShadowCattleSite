f0ffc1296b8589c0fcd3a2168f72a5e873abaf40
howellk3 <khowellmobile@gmail.com>
2023-11-27 10:42:16 -0500

    Added imports and started on homepage changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,28 @@
 import CenterTitle from "../components/content/CenterTitle";
 import Story from "../components/content/Story";
-
+import img0 from "../images/SunsetOnFarm.jpg";
 import classes from "./HomePage.module.css";
 import CardCarousel from "../components/content/CardCarousel";
 import MainNav from "../components/layout/MainNav";
 import Footer from "../components/layout/Footer";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function HomePage() {
   return (
     <div>
-      <div className={classes.parallaxContainer1}>
-        <MainNav />
-        <CenterTitle />
-      </div>
-      <div className={classes.buffer}>
-        <Story />
-      </div>
-      <div className={classes.parallaxContainer2}>
-        <h1 className={classes.carouselTitle}>Meet the Mammals</h1>
-        <CardCarousel />
-      </div>
-      <Footer />
+      <Parallax pages={2}>
+        <ParallaxLayer
+          offset={0}
+          //Something is wrong here. img0 reading in as text not variable
+          style={{ backgroundImage: 'url(${img0})', backgroundSize: "cover" }}
+        >
+          <MainNav />
+          <CenterTitle />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <h1>Bye</h1>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
