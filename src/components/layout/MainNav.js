dd33ff8e354bb4f2c3c3b219daefebbbf632b3bb
howellk3 <khowellmobile@gmail.com>
2023-11-30 10:18:12 -0500

    Adjusted menu

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -3,24 +3,40 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import LogoImg from "../../images/cowLogo.png";
+import classes from "./MainNav.module.css";
 
 function MainNav() {
   return (
     <Container fluid>
-      <Row xs={1} s={1} md={3} lg={3} className="py-4">
-        <Col xs={12} s={12} md={6} lg={6} className="text-center">
-          <div>
-            <h1 className="text-white">Shadyview Farms</h1>
+      <Row xs={1} s={5} md={5} lg={5} className="py-4">
+        <Col xs={12} s={1} md={1} lg={1} className="text-center">
+          <div className={`${classes.logoDiv} ${classes.outline}`}>
+            <img
+              className={`${classes.logo} ${classes.blackAndWhiteImage}`}
+              src={LogoImg}
+              alt="hello"
+            />
           </div>
         </Col>
-        <Col xs={12} s={12} md={3} lg={3} className="text-center">
+        <Col xs={12} s={5} md={5} lg={5} className="text-center">
+          <div className={`${classes.outline} ${classes.centered}`}>
+            <h1 className="text-white">Moonshadow Cattle</h1>
+          </div>
+        </Col>
+        <Col xs={12} s={2} md={2} lg={2} className="text-center">
           <Link to="/" className="text-decoration-none text-white">
-            <h2>Home</h2>
+            <h2 className={`${classes.outline} ${classes.centered}`}>Home</h2>
           </Link>
         </Col>
-        <Col xs={12} s={12} md={3} lg={3} className="text-center">
+        <Col xs={12} s={2} md={2} lg={2} className="text-center">
           <Link to="/about" className="text-decoration-none text-white">
-            <h2>About</h2>
+            <h2 className={`${classes.outline} ${classes.centered}`}>About</h2>
+          </Link>
+        </Col>
+        <Col xs={12} s={2} md={2} lg={2} className="text-center">
+          <Link to="/about" className="text-decoration-none text-white">
+            <h2 className={`${classes.outline} ${classes.centered}`}>Sister Site</h2>
           </Link>
         </Col>
       </Row>
