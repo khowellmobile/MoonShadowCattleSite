306c0c768c75654d31f871898cc6023b47e5ba74
howellk3 <khowellmobile@gmail.com>
2023-12-06 09:47:44 -0500

    Added Main Nav and background Picture

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,16 @@
+import MainNav from "../components/layout/MainNav";
+import classes from "./AboutPage.module.css";
+
+import Card from "react-bootstrap/Card";
+
 function AboutPage() {
-  return <div><h1>AboutPage</h1></div>;
+  return (
+    <div className={classes.topDiv} style={{ backgroundColor: "black" }}>
+      <MainNav />
+      <div>hi</div>
+      <div>bye</div>
+    </div>
+  );
 }
 
 export default AboutPage;
