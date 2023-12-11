00158ff39f03e700e4e30e48ed69ebc006b7c9cf
howellk3 <khowellmobile@gmail.com>
2023-12-11 09:55:39 -0500

    added cards

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,42 @@
 import MainNav from "../components/layout/MainNav";
 import classes from "./AboutPage.module.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import Card from "react-bootstrap/Card";
+import img0 from '../images/Gertrude.jpg'
 
 function AboutPage() {
   return (
     <div className={classes.topDiv}>
       <MainNav />
-      <div>hi</div>
-      <div>bye</div>
+      <Container fluid>
+        <Row xs={1} s={1} md={2} lg={2}>
+          <Col xs={12} s={12} md={6} lg={6}>
+            <Card className={classes.card}>
+              <Card.Img  className={classes.imgPreset1} variant="top" src={img0} />
+            </Card>
+          </Col>
+          <Col xs={12} s={12} md={6} lg={6}></Col>
+        </Row>
+        <Row xs={1} s={1} md={2} lg={2}>
+          <Col xs={12} s={12} md={6} lg={6}></Col>
+          <Col xs={12} s={12} md={6} lg={6}>
+            <Card className={classes.card}>
+            <Card.Img className={classes.imgPreset1} variant="top" src={img0} />
+            </Card>
+          </Col>
+        </Row>
+        <Row xs={1} s={1} md={2} lg={2}>
+          <Col xs={12} s={12} md={6} lg={6}>
+            <Card className={classes.card}>
+            <Card.Img className={classes.imgPreset1} variant="top" src={img0} />
+            </Card>
+          </Col>
+          <Col xs={12} s={12} md={6} lg={6}></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
