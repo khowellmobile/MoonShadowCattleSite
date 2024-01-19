4fe5e0bef7e21578ee3c26c60a05aee72e400774
howellk3 <khowellmobile@gmail.com>
2024-01-19 10:35:57 -0500

    Updated about page responsivness

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -24,20 +24,22 @@ function AboutPage() {
             </Card>
           </Col>
           <Col xs={12} s={12} md={6} lg={6} class='text-center'>
-            <h1 className={classes.bodyHeader}> Hello </h1>
-            <p className={classes.bodyText}>
-              Welcome to our farm, where Farmer John's lifelong dedication to
-              sustainable agriculture comes to life. With weathered hands and a
-              deep connection to the land, John cultivates more than just crops;
-              he nurtures a sense of community. Born into a farming legacy, his
-              days are spent working the fields, embracing the challenges with a
-              profound understanding of the cycles of nature. Beyond the farm
-              gates, John believes in sharing the harvest, fostering community
-              bonds, and sowing seeds of sustainability for generations to come.
-              Join us in celebrating Farmer John's story, where every furrow
-              tells a tale of resilience, passion, and a genuine love for the
-              land.
-            </p>
+            <div className={classes.contentDiv}>
+              <h1 className={classes.bodyHeader}> Hello </h1>
+              <p className={classes.bodyText}>
+                Welcome to our farm, where Farmer John's lifelong dedication to
+                sustainable agriculture comes to life. With weathered hands and
+                a deep connection to the land, John cultivates more than just
+                crops; he nurtures a sense of community. Born into a farming
+                legacy, his days are spent working the fields, embracing the
+                challenges with a profound understanding of the cycles of
+                nature. Beyond the farm gates, John believes in sharing the
+                harvest, fostering community bonds, and sowing seeds of
+                sustainability for generations to come. Join us in celebrating
+                Farmer John's story, where every furrow tells a tale of
+                resilience, passion, and a genuine love for the land.
+              </p>
+            </div>
           </Col>
         </Row>
         <Row xs={1} s={1} md={2} lg={2} className={classes.aboutRow}>
