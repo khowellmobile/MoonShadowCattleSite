bd71cfdbfe92e27232a6ca7d375256a5398e1fc2
howellk3 <khowellmobile@gmail.com>
2024-02-19 09:49:49 -0500

    Added basic bootstrap and cards to cattle page

--- src/pages/CattlePage.js
+++ src/pages/CattlePage.js
@@ -1,12 +1,33 @@
 import classes from "./CattlePage.module.css";
+
 import MainNav from "../components/layout/MainNav";
 import Footer from "../components/layout/Footer";
+import CattleCard from "../components/content/CattleCard";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function CattlePage() {
   return (
     <div>
       <MainNav />
-      <h1>CattlePage</h1>
+      <Container fluid>
+        <Row xs={2} s={2} md={4} lg={4} className={classes.aboutRow}>
+          <Col xs={6} s={6} md={3} lg={3}>
+            <CattleCard />
+          </Col>
+          <Col xs={6} s={6} md={3} lg={3}>
+            <CattleCard />
+          </Col>
+          <Col xs={6} s={6} md={3} lg={3}>
+            <CattleCard />
+          </Col>
+          <Col xs={6} s={6} md={3} lg={3}>
+            <CattleCard />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
